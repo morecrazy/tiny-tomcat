0550fe19c20528d498e2dd6f50ec68f85df89b72
zhqn3720 <zhqn3720@gmail.com>
2014-12-01 20:54:31 +0800

    init

---
+++ src/main/java/com/morecrazy/tomcat/App.java
@@ -0,0 +1,13 @@
+package com.morecrazy.tomcat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
